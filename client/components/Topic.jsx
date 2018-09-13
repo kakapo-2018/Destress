372962676e803c622b6148db3052ff5ff77ee18d
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-09-14 10:35:21 +1200

    Testing

--- client/components/Topic.jsx
+++ client/components/Topic.jsx
@@ -9,9 +9,24 @@ class Topic extends React.Component {
 render() {
   return (
     <div>
-      <h1>  circles here </h1>
-<Circle />
-</div>
+      <div id="return">
+      <button>Back</button>
+      </div>
+      <div id ="image">
+      <h1>I am Topic </h1>
+      </div>
+      {/* <div id="captions">
+      <div id ="captions-read">
+      <Caption />
+      </div>
+      <div id ="captions-add">
+      <AddCaption />
+      </div> */}
+      {/* <div id ="add-image">
+      
+      </div> */}
+      
+    </div>
   )
 }
 }
