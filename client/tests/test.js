a3f1f64c59adbb666d5cee7cfa482b79ac450809
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-09-14 12:00:42 +1200

    test1

--- client/tests/test.js
+++ client/tests/test.js
@@ -1,11 +1,12 @@
-
+configure({adapter: new Adapter()})
 import React from 'react'
-import {shallow} from 'enzyme'
-
+import {shallow, configure} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import Circle from '../components/Circle'
 
 test('<Circle /> has div', () => {
-  const expected = "circles"
+  const expected = "Circles component"
   const wrapper = shallow(<Circle />)
-  expect(wrapper.find("circles")).toBe(expected)
+  console.log(wrapper)
+  expect(wrapper.text()).toMatch(expected)
 })
